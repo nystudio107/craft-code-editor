7010c332d91d36c63705bd4e5b0551cf495cd730
Andrew Welch <andrew@nystudio107.com>
2023-04-12 14:08:32 -0400

    chore: Updated assets build

--- src/web/assets/dist/js/vendors.js
+++ src/web/assets/dist/js/vendors.js
@@ -2,7 +2,7 @@
  * @project        code-editor
  * @name           vendors.js
  * @author         Andrew Welch
- * @build          Wed Apr 12 2023 01:16:18 GMT+0000 (Coordinated Universal Time)
+ * @build          Wed Apr 12 2023 18:03:17 GMT+0000 (Coordinated Universal Time)
  * @copyright      Copyright (c) 2023 ©2022 nystudio107.com
  *
  */
