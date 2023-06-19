47cca88c19a3322d8281fab013f662ab06436956
Ben Croker <57572400+bencroker@users.noreply.github.com>
2023-06-19 15:49:56 +0200

    Fix API docs URL

--- src/web/assets/src/js/default-monaco-options.ts
+++ src/web/assets/src/js/default-monaco-options.ts
@@ -1,5 +1,5 @@
 // The default EditorOptions for the Monaco editor instance
-// ref: https://microsoft.github.io/monaco-editor/api/interfaces/monaco.editor.IEditorOptions.html
+// ref: https://microsoft.github.io/monaco-editor/typedoc/interfaces/editor.IEditorOptions.html
 import * as monaco from "monaco-editor";
 
 export const defaultMonacoOptions: monaco.editor.IStandaloneEditorConstructionOptions = {
